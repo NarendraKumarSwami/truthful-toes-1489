502628e1664e100b8ce884030c2b60e1ba5bc4a2
NarendraKumarswami <narendrakumarswami786@gmail.com>
2022-12-18 08:50:21 +0530

    dao and model pacakage is updated

---
+++ fastbiding/src/com/fastbiding/usecase/SiteAdminUseCase.java
@@ -0,0 +1,14 @@
+package com.fastbiding.usecase;
+
+import com.fastbiding.dao.SiteAdminDao;
+import com.fastbiding.dao.SiteAdminDaoImpl;
+
+public class SiteAdminUseCase {
+         // All Usecasse for SiteAdmin defined using methods
+	     
+	public SiteAdminDao dao = new SiteAdminDaoImpl(); 
+	
+	public void login() {
+		       
+	}
+}
